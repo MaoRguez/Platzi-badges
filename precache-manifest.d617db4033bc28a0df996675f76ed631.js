3ede125d2ab791e6003584b4d5f107a02386b31f
Maorguez <avila.mauro@gmail.com>
2020-07-29 21:43:37 -0500

    Updates

---
+++ precache-manifest.d617db4033bc28a0df996675f76ed631.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5206b6c63b5ad50dba04e13577c2af",
+    "url": "/Platzi-badges/index.html"
+  },
+  {
+    "revision": "35f540f04c36451b4e21",
+    "url": "/Platzi-badges/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "de5a1e9fc65168f8f3d1",
+    "url": "/Platzi-badges/static/css/main.4eb34a24.chunk.css"
+  },
+  {
+    "revision": "35f540f04c36451b4e21",
+    "url": "/Platzi-badges/static/js/2.1c4a2615.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/Platzi-badges/static/js/2.1c4a2615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5a1e9fc65168f8f3d1",
+    "url": "/Platzi-badges/static/js/main.ea36d7fc.chunk.js"
+  },
+  {
+    "revision": "8c48fad44a78c26d6165",
+    "url": "/Platzi-badges/static/js/runtime-main.e45eb1f7.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/Platzi-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/Platzi-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/Platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/Platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Platzi-badges/static/media/stars.1d17360a.svg"
+  }
+]);
